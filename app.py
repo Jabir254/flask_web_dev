d54fed304df669705e7ee73e4f5f4474dfd2f875
jabir254 <jabirkullow@gmail.com>
2023-07-13 09:16:19 +0300

    added list of dictionary

--- app.py
+++ app.py
@@ -2,6 +2,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Scientist',
+        'location':'Nairobi',
+        'salary': 'Kes. 400000'
+    },
+    {
+        'id': 2,
+        'title': 'Backend Engineer',
+        'location':'Nairobi',
+        'salary': 'Kes. 350000'
+    },
+    {
+        'id': 3,
+        'title': 'Web Developer',
+        'location':'Nairobi',
+        'salary': 'Kes. 300000'
+    },
+    {
+        'id': 4,
+        'title': 'Data Analyst',
+        'location':'Nairobi',
+        'salary': 'Kes. 250000'
+    }
+]
+
 @app.route("/")
 def hello_world():
     return render_template('home.html')
